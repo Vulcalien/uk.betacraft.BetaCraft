9b008c2848e39d90c0dcc2b52f7d28ba8edd6daa
Vulcalien <vulcalien@vulcalien.net>
2024-02-20 14:38:48 +0100

    betacraft.sh: surround arguments in quotes

--- betacraft.bash
+++ betacraft.bash
@@ -3,7 +3,7 @@
 # needed for Discord RP
 # source: https://github.com/flathub/com.discordapp.Discord/wiki/Rich-Precense-(discord-rpc)#flatpak-applications
 for i in {0..9}; do
-    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+    test -S "$XDG_RUNTIME_DIR/discord-ipc-$i" || ln -sf "app/com.discordapp.Discord/discord-ipc-$i" "$XDG_RUNTIME_DIR/discord-ipc-$i";
 done
 
 exec java -jar /app/share/launcher.jar
